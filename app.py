599df434e70f1c8724941d5c8269157d083c2046
Danyyl Dolynsky <dolynskydanyyl@gmail.com>
2023-02-16 12:56:50 +0200

    added test data

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from models import db
-from views.admin import admin
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config')
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{app.config.from_envvar['USER']}:" \
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"{app.config.from_envvar['DB']}://{app.config.from_envvar['USER']}:" \
                                             f"{app.config.from_envvar['PASSWORD']}" \
                                             f"@{app.config.from_envvar['DB_NAME']}" \
-                                            f":port/{app.config.from_envvar['USER']}"
+                                            f":port/{app.config.from_envvar['HOST']}"
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
 
+    from views.admin import admin
     app.register_blueprint(admin)
 
     return app
--- views/admin.py
+++ views/admin.py
@@ -4,6 +4,7 @@ from sqlalchemy_utils import database_exists, create_database
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
+
 engine = create_engine(current_app.config['SQLALCHEMY_DATABASE_URI'])
 if not database_exists(engine.url):
     create_database(engine.url)
